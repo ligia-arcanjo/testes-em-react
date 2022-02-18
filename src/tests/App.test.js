caa56167c10ba2ede391f3c268458387b7df9c3d
Ligia <ligiaarcanjo84@gmail.com>
2022-02-18 18:39:16 -0300

    implementação do teste para verificar renderização das rotas

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
 describe('Testa o componente <App.js />', () => {
-  test('Verifica se a página inicial contém um conjunto de links de navegação', () => {
+  it('Verifica se a página inicial contém um conjunto de links de navegação', () => {
     renderWithRouter(<App />);
     const homeLinkEl = screen.getByRole('link', { name: /Home/i });
     const aboutLinkEl = screen.getByRole('link', { name: /About/i });
     const favoriteLinkEl = screen.getByRole('link', { name: /Favorite Pokémons/i });
     expect(homeLinkEl && aboutLinkEl && favoriteLinkEl).toBeInTheDocument();
+  });
+
+  it('Verifica se é redirecionado para / ao clicar no link Home.', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLinkEl = screen.getByRole('link', { name: /Home/i });
+
+    userEvent.click(homeLinkEl);
+    expect(history.location.pathname).toEqual('/');
+  });
+
+  it('Verifica se é redirecionado para /about ao clicar no link About.', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLinkEl = screen.getByRole('link', { name: /About/i });
+
+    userEvent.click(aboutLinkEl);
+    expect(history.location.pathname).toEqual('/about');
+  });
+
+  it('Verifica se é redirecionado para /favorites ao clicar no link Favorite.', () => {
+    const { history } = renderWithRouter(<App />);
+    const favoriteLinkEl = screen.getByRole('link', { name: /Favorite Pokémons/i });
+
+    userEvent.click(favoriteLinkEl);
+    expect(history.location.pathname).toEqual('/favorites');
   });
 });
