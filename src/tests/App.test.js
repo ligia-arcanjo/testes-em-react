a1783e1a3ade613b8c551b9abe9f887eb1a00c31
Ligia <ligiaarcanjo84@gmail.com>
2022-02-18 17:41:20 -0300

    implementação do teste para os links de navegação

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente <App.js />', () => {
+  test('Verifica se a página inicial contém um conjunto de links de navegação', () => {
+    renderWithRouter(<App />);
+    const homeLinkEl = screen.getByRole('link', { name: /Home/i });
+    const aboutLinkEl = screen.getByRole('link', { name: /About/i });
+    const favoriteLinkEl = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(homeLinkEl && aboutLinkEl && favoriteLinkEl).toBeInTheDocument();
+  });
+});
