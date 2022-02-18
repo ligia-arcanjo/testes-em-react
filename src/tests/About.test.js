6d2084420e9037d9b9bf834886aa9bf4b439e164
Ligia <ligiaarcanjo84@gmail.com>
2022-02-18 20:48:17 -0300

    implementação do teste da renderização das tags <p>  no <About />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,7 +4,20 @@ import renderWithRouter from './renderWithRouter';
 import { About } from '../components';
 
 describe('Testa o componente <About.js />', () => {
-  it('Verifica se a página contém um heading <h2> com o texto "About Pokédex".', () => {
+  it('Verifica se a página contém as informações sobre a Pokédex e dois parágrafos',
+    () => {
+      renderWithRouter(<About />);
+      const paragraphEl1 = screen.getByText(/This application simulates a Pokédex/i);
+      const paragraphEl2 = screen.getByText(
+        /a digital encyclopedia containing all Pokémons/i,
+      );
+      const paragraphEl3 = screen.getByText(
+        /One can filter Pokémons by type, and see more details for each one of them/i,
+      );
+      expect(paragraphEl1 && paragraphEl2 && paragraphEl3).toBeInTheDocument();
+    });
+
+  it('Verifica se a página contém um heading <h2> com o texto "About Pokédex.', () => {
     renderWithRouter(<About />);
     const titleEl = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
     expect(titleEl).toBeInTheDocument();
