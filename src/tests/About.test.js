89d247c8f0bc103abd3396ec880f9ab6c0ea57ce
Ligia <ligiaarcanjo84@gmail.com>
2022-02-18 19:13:37 -0300

    implementação do teste heading h2 no <About />

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => { });
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { About } from '../components';
+
+describe('Testa o componente <About.js />', () => {
+  it('Verifica se a página contém um heading <h2> com o texto "About Pokédex".', () => {
+    renderWithRouter(<About />);
+    const titleEl = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(titleEl).toBeInTheDocument();
+  });
+});
