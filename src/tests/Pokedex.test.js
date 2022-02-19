e4a9f12b3eca04e5ea5dcd3952db0dc276e33714
Ligia <ligiaarcanjo84@gmail.com>
2022-02-19 13:27:22 -0300

    implementação do teste para h2 no componente Pokedex.js

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente Pokedex.js', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  it('Verifica se página contém um heading h2 com o texto "Encountered pokémons".',
+    () => {
+      const titleEl = screen.getByRole('heading', { name: /Encountered pokémons/i,
+        level: 2 });
+      expect(titleEl).toBeInTheDocument();
+    });
+});
